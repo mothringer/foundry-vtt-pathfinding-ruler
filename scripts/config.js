c8ab6d4a1f649268e4dbbaaef41ddf246837649c
John Haverkamp <john@haverkamp.us>
2020-10-28 19:45:07 -0500

    Another attempt at config.

--- scripts/config.js
+++ scripts/config.js
@@ -1,23 +1,17 @@
-class Config {
-  constructor() {
-    game.settings.register("grid-pathfinder", "pathfinderDiagonals", {
-      name: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.name"),
-      hint: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.hint"),
-      scope: "world",
-      config: true,
-      default: true,
-      type: Boolean,
-    });
+game.settings.register("grid-pathfinder", "pathfinderDiagonals", {
+	name: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.name"),
+	hint: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.hint"),
+	scope: "world",
+	config: true,
+	default: true,
+	type: Boolean,
+});
 
-    game.settings.register("grid-pathfinder", "printMoveToChat", {
-      name: game.i18n.localize("grid-pathfinder.printMoveToChat.name"),
-      hint: game.i18n.localize("grid-pathfinder.printMoveToChat.hint"),
-      scope: "world",
-      config: true,
-      default: true,
-      type: Boolean,
-    });
-  }
-}
-
-export default Config;
+game.settings.register("grid-pathfinder", "printMoveToChat", {
+	name: game.i18n.localize("grid-pathfinder.printMoveToChat.name"),
+	hint: game.i18n.localize("grid-pathfinder.printMoveToChat.hint"),
+	scope: "world",
+	config: true,
+	default: true,
+	type: Boolean,
+});
