e39454c8589e19ea787ac223b12fe1792fadfd45
John Haverkamp <john@haverkamp.us>
2020-10-30 13:27:51 -0500

    Name change

---
+++ scripts/config.js
@@ -0,0 +1,40 @@
+/*	Copyright (C) 2020 John Haverkamp
+* 
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU Affero General Public License as published
+*   by the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU Affero General Public License for more details.
+*
+*   You should have received a copy of the GNU Affero General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+class Config {
+	constructor()
+	{/* to support planned features
+		game.settings.register("grid-pathfinder", "pathfinderDiagonals",
+		{
+			name: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.name"),
+			hint: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.hint"),
+			scope: "world",
+			config: true,
+			default: true,
+			type: Boolean,
+		});
+	
+		game.settings.register("grid-pathfinder", "printMoveToChat",
+		{
+			name: game.i18n.localize("grid-pathfinder.printMoveToChat.name"),
+			hint: game.i18n.localize("grid-pathfinder.printMoveToChat.hint"),
+			scope: "world",
+			config: true,
+			default: true,
+			type: Boolean,
+		});*/
+	}
+}
--- scripts/main.js
+++ scripts/main.js
@@ -15,30 +15,7 @@
 */
 "use strict"
 
-class Config {
-	constructor()
-	{/* to support planned features
-		game.settings.register("grid-pathfinder", "pathfinderDiagonals",
-		{
-			name: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.name"),
-			hint: game.i18n.localize("grid-pathfinder.pathfinderDiagonals.hint"),
-			scope: "world",
-			config: true,
-			default: true,
-			type: Boolean,
-		});
-	
-		game.settings.register("grid-pathfinder", "printMoveToChat",
-		{
-			name: game.i18n.localize("grid-pathfinder.printMoveToChat.name"),
-			hint: game.i18n.localize("grid-pathfinder.printMoveToChat.hint"),
-			scope: "world",
-			config: true,
-			default: true,
-			type: Boolean,
-		});*/
-	}
-}
+import Config from "./config";
 
 class PathFinder {
 	constructor()
